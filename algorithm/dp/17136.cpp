62d7d302b43da6fef3acedf1a07eb70c20beba92
rlawlgh0813 <jihooo7@naver.com>
2025-12-30 20:20:14 +0900

    17136

--- algorithm/dp/17136.cpp
+++ algorithm/dp/17136.cpp
@@ -1,32 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 1e9
-int a[12], block[6];
-int ret = INF;
 
-void go(int x, int y){
-    if(x == 9 && y == 9){
-        int cnt = 0;
-        for(int i=1; i<6; i++) cnt += block[i];
-        ret = min(ret, cnt);
-    }
-    if(!a[x][y]) return;
-    for()
-}
+int board[10][10];
+int paper[6];              // 각 크기별 사용 수
+int best_ans = 1e9;
 
-void solve(){
-    for(int i=0; i<10; i++){
-        for(int j=9; j>=0; j--){
-            int t; cin >> t;
-            if(t) a[i] |= (1<<j);
+bool canPlace(int x, int y, int sz) {
+    if (x + sz > 10 || y + sz > 10) return false;
+    for (int i = 0; i < sz; i++) {
+        for (int j = 0; j < sz; j++) {
+            if (board[x + i][y + j] == 0) return false;
         }
     }
-    go(0,0);
-    cout << ((ret == INF) ? -1 : ret);
+    return true;
 }
 
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    solve();
+void place(int x, int y, int sz, int val) {
+    for (int i = 0; i < sz; i++) {
+        for (int j = 0; j < sz; j++) {
+            board[x + i][y + j] = val;
+        }
+    }
+}
+
+void dfs(int used) {
+    if (used >= best_ans) return;  // 현재 최선보다 많으면 중단
+
+    int sx = -1, sy = -1;
+    for (int i = 0; i < 10 && sx == -1; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (board[i][j]) {
+                sx = i; sy = j;
+                break;
+            }
+        }
+    }
+
+    if (sx == -1) {                // 덮을 곳 없음 = 완료
+        best_ans = min(best_ans, used);
+        return;
+    }
+
+    for (int sz = 5; sz >= 1; sz--) {
+        if (paper[sz] >= 5) continue;
+        if (!canPlace(sx, sy, sz)) continue;
+
+        place(sx, sy, sz, 0);
+        paper[sz]++;
+        dfs(used + 1);
+        paper[sz]--;
+        place(sx, sy, sz, 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) cin >> board[i][j];
+    }
+
+    dfs(0);
+    cout << (best_ans == 1e9 ? -1 : best_ans);
     return 0;
 }
